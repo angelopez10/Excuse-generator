10ab01c362895b5903acf597ec7b431166aec4fe
angelopez10 <angelopez10898@gmail.com>
2020-01-07 20:15:04 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let onload = document.getElementById('excuse');
+
+
+let who = ['My mom', 'The teacher', 'The bus driver', 'My bestfriend']
+let what = ['eat', 'crushed', 'broked', 'pissed']
+let when = ['before the class','right in time','when I finished','during my lunch','while I was praying']
+
+
+function random_item(items)
+{
+  
+return items[Math.floor(Math.random()*items.length)];
+     
+}
+
+var resultado = (random_item(who) + ' '+ random_item(what) + ' ' +random_item(when));
+
+
+onload.innerHTML = resultado;
+
+    
+
+
+
+    
+
+
+
+
